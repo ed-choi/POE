90259f7cb0e9d0f7171c8ed09289e21d87e094f4
Matt Robinson <mrobinson7627@gmail.com>
2016-07-29 23:38:11 -0400

    Item parsing now works

--- parser.js
+++ parser.js
@@ -1,62 +1,55 @@
 require('pointfree-fantasy').expose(global);
 
-const fs = require('fs');
-
 const _ = require('lodash');
 const parse = require('bennu').parse;
 const text = require('bennu').text;
-const stream = require('nu-stream').stream;
 
-const streamStr = compose(mconcat, stream.toArray);
+// this syntax makes me sad
+const eagerConcat = compose(map(mconcat), parse.eager);
+
+// consume p and resolve with (pure) x if it is successful
+const ensure = p => x => parse.next(p, parse.of(x));
 
 const spaces = parse.many1(text.space);
 
-const name = parse.either(
-    text.string('spoiler'),
-    text.string('item')
-);
-
-const value = parse.eager(parse.many1(
+const stringValue = eagerConcat(parse.many1(
     text.noneOf('"')
 ));
-
-const keyValue = k => text.string(k)
-    .chain(__ => text.string('="'))
-    .chain(__ => value)
-    .chain(v => parse.next(
-        text.character('"'),
-        parse.of(_.fromPairs([[k, v]]))
-    ));
 
 const outerSpoiler = text.string('[spoiler]');
 
 const endSpoiler = text.string('[/spoiler]');
 
-const price = parse.eager(parse.many1(
-    text.noneOf('"')
-));
-
+// [spoiler=" ~b/o 9 chaos"]
 const priceSpoiler = text.string('[spoiler="')
-    .chain(__ => price)
-    .chain(p => parse.next(
-        text.string('"]'),
-        parse.of(p)
+    .chain(__ => stringValue)
+    .chain(ensure(text.string('"]')));
+
+// key="value"
+const keyValue = k => text.string(k)
+    .chain(__ => text.string('="'))
+    .chain(__ => stringValue)
+    .chain(v => parse.next(
+        text.character('"'),
+        parse.of(_.fromPairs([[k, v]]))
     ));
 
+const post = keyValue('post');
+const index = keyValue('index');
+const inline = keyValue('inline');
+
+// [item post="1234" index="5" inline="432"]
 const item = text.string('[item')
     .chain(__ => spaces)
-    .chain(__ => post.chain(__ => spaces)
-        .chain(p => index.chain(__ => spaces)
-        .chain(i => inline.chain(__ => spaces)
-        .chain(inl => parse.of(_.assign({}, p, i, inl)))
+    .chain(__ => post)
+    .chain(p => spaces.chain(__ => index)
+        .chain(i => spaces.chain(__ => inline)
+        .chain(inl => parse.of(_.assign({}, p, i, inl))
     )))
-    .chain(it => parse.next(
-        parse.character(']'),
-        it
-    ));
+    .chain(ensure(text.character(']')));
 
 const main = () => {
-    const res = parse.run(bracket, '[ab/]');
+    const res = parse.run(item, '[item post="1234" index="5" inline="432"]');
     console.log(res);
 };
 
